6a15db3e6bb2e28682c40da41d88c86fd239b60b
Morpa <andremorpa@gmail.com>
2023-12-19 18:49:21 +0000

    add create user

---
+++ models/user.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"github.com/Morpa/go-rest-api/db"
+	"github.com/Morpa/go-rest-api/utils"
+)
+
+type User struct {
+	ID       int64
+	Email    string `binding:"required"`
+	Password string `binding:"required"`
+}
+
+func (u User) Save() error {
+	query := "INSERT INTO users(email, password) VALUES (?, ?)"
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	hashPassword, err := utils.HasPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashPassword)
+
+	if err != nil {
+		return err
+	}
+
+	userId, err := result.LastInsertId()
+
+	u.ID = userId
+	return err
+}
